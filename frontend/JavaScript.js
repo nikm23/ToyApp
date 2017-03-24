bd1e6c02c1151e142ed83dd35cdf319cf94602fd
nikm23 <mangla.nikhil@gmail.com>
2017-03-24 15:52:16 +0530

    JS tute

---
+++ frontend/JavaScript.js
@@ -0,0 +1,28 @@
+function validateForm() {
+	var mess="";
+	var ret = true;
+	var x=document.forms["myForm"]["firstname"].value;
+		if (x==""){
+			mess=mess+"Name must be filled out";
+			ret= false;
+		}
+	var x=document.forms["myForm"]["lastname"].value;
+		if (x==""){
+			mess+="\nLast Name must be filled out";
+			ret = false;
+		}
+	var x=document.forms["myForm"]["address"].value;
+		if (x==""){
+			mess+="\nAddress must be filled out";
+			ret = false;
+		}
+
+	if(ret == false)
+	{
+		alert(mess);
+		document.getElementById('myForm').reset();
+		document.myForm.firtname.focus();
+		return false;
+	}
+
+}
